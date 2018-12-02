3b8ca2bc7a1482d1404a086cde88d93c285fb803
Andrey Chausov <dxahtepb@gmail.com>
2018-12-02 03:26:35 +0300

    bug fix

--- ITMO/NewShinyNaiveBayes/Metrics.py
+++ ITMO/NewShinyNaiveBayes/Metrics.py
@@ -15,13 +15,30 @@ class Metrics:
                 conf_matrix[predicted_data[idx]][test_case] += 1
             return conf_matrix
 
-        conf_matrix = get_c_matrix(test_data,predicted_data,n_classes)
+        conf_matrix = get_c_matrix(test_data, predicted_data, n_classes)
         prec = 0
         rec = 0
+
         for i in range(n_classes):
-            prec += conf_matrix[i][i] / sum(conf_matrix[i])
-            rec += conf_matrix[i][i] / sum([conf_matrix[j][i] for j in range(len(conf_matrix))])
+            div = sum(conf_matrix[i])
+            if div == 0:
+                prec += 1
+            else:
+                prec += conf_matrix[i][i] / div
+
+            div = sum([conf_matrix[j][i] for j in range(len(conf_matrix))])
+            if div == 0:
+                rec += 1
+            else:
+                rec += conf_matrix[i][i] / div
+
         prec = prec / n_classes
         rec = rec / n_classes
         res_score = (pow(beta, 2) + 1) * (prec * rec) / (pow(beta, 2) * prec + rec)
+
         return res_score
+
+    @staticmethod
+    def f_score2(test_data, predicted_data):
+        from sklearn.metrics import f1_score
+        return f1_score(test_data, predicted_data)
